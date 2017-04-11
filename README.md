7c4c01a8d8b0c789e840486a0f9aa35ed665d868
mhawker <michael.hawker@outlook.com>
2017-04-11 12:19:29 -0700

    Add another custom mode using a custom icon loaded from a file.

---
+++ README.md
@@ -0,0 +1,19 @@
+Dial Events HWA Sample
+======================
+This sample shows how to integrate the [Microsoft Dial API](https://docs.microsoft.com/en-us/windows/uwp/input-and-devices/windows-wheel-interactions) into a Hosted Web App on Windows 10 Anniversary edition (or higher).
+
+**This Sample Demonstrates the following:**
+
+* Changing Default System Menu Items
+* Responding to Press and Rotation Events
+* Adding Custom Items from Image Files
+
+### Custom Icons
+When building a custom icon for a dial interaction, create a 64x64 transparent PNG.
+
+It should be white in the foreground with a black border (for high contrast mode).
+
+I found a border of 2px worked well with a margin of 7px per edge before image content.
+
+### LICENSE
+Released under the MIT License, see LICENSE file for full details.
--- js/main.ts
+++ js/main.ts
@@ -9,6 +9,13 @@ function log(msg)
     console.log(msg);
 }
 
+let mode = 'system';
+
+function modeSwitch(ev: Windows.Foundation.TypedEventHandlerArgumentType<Windows.UI.Input.RadialControllerMenuItem, Object>) {
+    log("invoked " + ev.target.displayText);
+    mode = ev.target.displayText;
+}
+
 if (typeof Windows != 'undefined')
 {
     log("initializing dial");
@@ -24,18 +31,21 @@ if (typeof Windows != 'undefined')
 
     // Add our own item to respond to
     var mi = Windows.UI.Input.RadialControllerMenuItem.createFromKnownIcon("Undo/Redo", Windows.UI.Input.RadialControllerMenuKnownIcon.undoRedo);
-    mi.addEventListener("invoked", (ev) => {
-        log("invoked " + ev.target.displayText);
-    });
+    mi.addEventListener("invoked", modeSwitch);
+
+    // Add a custom image item to the menu from our app package
+    var mi2 = Windows.UI.Input.RadialControllerMenuItem.createFromIcon("Custom", Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(new Windows.Foundation.Uri("ms-appx:///icons/gear.png")));
+    mi2.addEventListener("invoked", modeSwitch);
 
     controller.menu.items.push(mi);
+    controller.menu.items.push(mi2);
 
     controller.addEventListener("buttonclicked", (ev) => {
-        log("dial pressed");
+        log("dial pressed: " + mode);
     })
 
     controller.addEventListener("rotationchanged", (ev) => {
-        log("rotation changed: " + ev.detail[0].rotationDeltaInDegrees);
+        log("rotation changed: " + ev.detail[0].rotationDeltaInDegrees + " in " + mode);
     });
 
     // To remove system defaults and just have your own...
