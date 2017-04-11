caaf57412a74c87997874ecdf16a26a4606cef90
mhawker <michael.hawker@outlook.com>
2017-04-11 11:14:28 -0700

    Update to latest Typings for proper events. Show modification of system menu items.

--- js/main.ts
+++ js/main.ts
@@ -11,23 +11,35 @@ function log(msg)
 
 if (typeof Windows != 'undefined')
 {
+    log("initializing dial");
+
+    // Modify System Defaults to Only Show Volume and Next/Prev Track as per guidance.
+    // https://docs.microsoft.com/en-us/windows/uwp/input-and-devices/windows-wheel-interactions
+    var config = Windows.UI.Input.RadialControllerConfiguration.getForCurrentView();
+
+    config.setDefaultMenuItems(<any>[Windows.UI.Input.RadialControllerSystemMenuItemKind.volume,
+                                     Windows.UI.Input.RadialControllerSystemMenuItemKind.scroll]);
+
     var controller = Windows.UI.Input.RadialController.createForCurrentView();
 
     // Add our own item to respond to
     var mi = Windows.UI.Input.RadialControllerMenuItem.createFromKnownIcon("Undo/Redo", Windows.UI.Input.RadialControllerMenuKnownIcon.undoRedo);
-    mi.addEventListener('invoked', (ev) => {
+    mi.addEventListener("invoked", (ev) => {
         log("invoked " + ev.target.displayText);
     });
 
     controller.menu.items.push(mi);
 
-    controller.onbuttonclicked = (ev) => {
+    controller.addEventListener("buttonclicked", (ev) => {
         log("dial pressed");
-    };
+    })
 
-    controller.addEventListener('rotationchanged', (ev) => {
+    controller.addEventListener("rotationchanged", (ev) => {
         log("rotation changed: " + ev.detail[0].rotationDeltaInDegrees);
     });
+
+    // To remove system defaults and just have your own...
+    ////config.setDefaultMenuItems(<any>[]);
 
     log("win/dial initialized");
 }
